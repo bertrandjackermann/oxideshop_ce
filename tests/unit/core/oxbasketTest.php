13f08594a93529b0479fcd6eea60325372ac024e
hkreuter <heike.reuter@oxid-esales.com>
2016-02-02 13:00:49 +0100

    ESDEV-3338 0006204 Fix VAT calculation when percentaged voucher is applied
     Changes from PR311.

--- tests/unit/core/oxbasketTest.php
+++ tests/unit/core/oxbasketTest.php
@@ -4409,13 +4409,11 @@ class Unit_Core_oxbasketTest extends OxidTestCase
      */
     public function testForBugEntries5795_6204_6283()
     {
-        $sRegularArticleId = '1951';
-
+        $sRegularArticleId = '1952';
         $sDiscArticleId = '2024';
-
         $this->oVoucherSerie->oxvoucherseries__oxdiscounttype = new oxField('percent', oxField::T_RAW);
         $this->oVoucherSerie->save();
-        // assigning voucher serie to Article $sDiscArticleId
+        // assigning voucher serie to Article 2024
         $oDisc2Art = oxNew("oxBase");
         $oDisc2Art->init("oxobject2discount");
         $oDisc2Art->setId("_dsci1");
@@ -4433,9 +4431,8 @@ class Unit_Core_oxbasketTest extends OxidTestCase
         $oBasket->addVoucher($oVoucher->oxvouchers__oxvouchernr->value); // 10 %
         $oBasket->calculateBasket(false);
         $aVats = $oBasket->getProductVats(false);
-
-        $this->assertEquals(3.82, $aVats[19]);
-        $this->assertEquals(20.08, round($oBasket->getNettoSum(), 2));
+        $this->assertEquals(2.54, $aVats[19]);
+        $this->assertEquals(13.36, $oBasket->getNettoSum());
     }
 
     /**
